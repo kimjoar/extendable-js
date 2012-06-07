0422b6df8ed4e67286e1e00ffd748704a2891ca3
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2012-06-07 19:24:05 +0200

    No dependencies

--- extendable.js
+++ extendable.js
@@ -1,8 +1,9 @@
-// extendable.js v0.0.1
+// extendable.js v0.1.0
 //
-// extendable.js is a mixin to easily create a layered architecture. Let's say
-// you want to have a view component, but you want all views to inherit from a
-// parent, with the prototype chain properly set up. This can be done as follows:
+// extendable.js is a mixin to easily create a layered architecture.
+//
+// Let's say you want to have a view component, but you want all views to
+// inherit from a parent, with the prototype chain properly set up. Example:
 //
 //     var View = function() {
 //       // constructor
@@ -24,50 +25,62 @@
 //       }
 //     });
 //
-// extendable.js is based on [Backbone.js](http://backbonejs.org/)
+// extendable.js is based on [Backbone.js](http://backbonejs.org/) and
+// [Underscore.js](http://underscorejs.org/).
 //
 // Copyright 2012, Kim Joar Bekkelund MIT Licensed
 
-var extendable = (function($) {
+var extendable = (function() {
+
+  var add = function(obj) {
+    var objects = Array.prototype.slice.call(arguments, 1);
+
+    for (var i = 0, l = objects.length; i < l; i++) {
+      var source = objects[i];
+      for (var prop in source) {
+        obj[prop] = source[prop];
+      }
+    }
+
+    return obj;
+  };
 
   var ctor = function(){};
 
-  var extend = function(properties, staticProperties) {
-    var parent = this;
-
-    // Create child constructor
-    var child = function() {
-      // … which only job is to call the parent construtor with all
-      // the arguments
-      parent.apply(this, arguments);
-    };
-
-    // Inherit class (static) properties from parent.
-    $.extend(child, parent);
-
-    // Set the prototype chain to inherit from `parent`
-    ctor.prototype = parent.prototype;
-    child.prototype = new ctor();
-
-    // Add prototype properties, i.e. instance properties
-    if (properties) $.extend(child.prototype, properties);
-
-    // Add static properties, i.e. class properties
-    if (staticProperties) $.extend(child, staticProperties);
-
-    // Correctly set child's prototype.constructor.
-    child.prototype.constructor = child;
-
-    // The child must also be able to create new subclasses
-    child.extend = parent.extend;
-
-    return child;
-  };
-
   return function() {
-    this.extend = extend;
+    this.extend = function(properties, staticProperties) {
+      var parent = this;
+
+      // Create child constructor
+      var child = function() {
+        // … which only job is to call the parent construtor with all
+        // the arguments
+        parent.apply(this, arguments);
+      };
+
+      // Inherit class (static) properties from parent.
+      add(child, parent);
+
+      // Set the prototype chain to inherit from `parent`
+      ctor.prototype = parent.prototype;
+      child.prototype = new ctor();
+
+      // Add prototype properties, i.e. instance properties
+      if (properties) add(child.prototype, properties);
+
+      // Add static properties, i.e. class properties
+      if (staticProperties) add(child, staticProperties);
+
+      // Correctly set child's prototype.constructor.
+      child.prototype.constructor = child;
+
+      // The child must also be able to create new subclasses
+      child.extend = parent.extend;
+
+      return child;
+    };
 
     return this;
   };
 
-})(jQuery);
+})();
--- spec/buster.js
+++ spec/buster.js
@@ -3,9 +3,6 @@ var config = module.exports;
 config["My tests"] = {
     rootPath: "../",
     environment: "browser",
-    libs: [
-        "lib/jquery-1.7.2.js",
-    ],
     src: ["extendable.js"],
     specs: ["spec/*-spec.js"]
 };
