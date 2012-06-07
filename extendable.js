8ced7b7222e83d16b445d831bfb40608f88205e4
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2012-06-07 19:46:13 +0200

    Better description

--- extendable.js
+++ extendable.js
@@ -1,38 +1,79 @@
 // extendable.js v0.1.0
 //
-// extendable.js is a mixin to easily create a layered architecture.
+// extendable.js is a mixin used to easily create a layered architecture.
 //
-// Let's say you want to have a view component, but you want all views to
-// inherit from a parent, with the prototype chain properly set up. Example:
+// The code follows a functional mixins pattern described by Angus Croll in his
+// [article on JavaScript mixins](http://javascriptweblog.wordpress.com/2011/05/31/a-fresh-look-at-javascript-mixins/).
+//
+// Usage
+// -----
+//
+// In browsers include a single JavaScript file:
+//
+//     <script src="extendable-0.1.0.js"></script>/
+//
+// Example usage
+// -------------
+//
+// Define the constructor which will make use of extendable:
 //
 //     var View = function() {
 //       // constructor
 //     }
-//     View.prototype.instanceMethod = function() {
-//       // instance method
-//     }
+//
+// This constructor will be called whenever a child is initialized.
+//
+// You can then add class and instance properties to your parent using regular
+// JavaScript:
+//
+//     View.classProperty = function() {};
+//
+//     View.prototype.instanceProperty = function() {}
+//
+// Add (mixin) extendable functionality to `View`:
+//
 //     extendable.call(View);
 //
-//     // Now new views can be created using `extend`:
+// `View` will now have access to an `extend` method, which can be used to
+// create new components, optionally passing instance and class properties:
 //
 //     var UserView = View.extend({
-//       someMethod: function() {
-//         // an example of an instance method
+//       instanceTest: function() {
+//         // an example of an instance property
 //       }
 //     }, {
-//       someStaticMethod: function() {
-//         // an example of a static method
+//       classTest: function() {
+//         // an example of a class property
 //       }
 //     });
 //
+// This class can now be initialized:
+//
+//     var view = new UserView();
+//
+// And we can call properties on the newly created instance:
+//
+//     view.instanceTest();
+//
+// We can also call properties available on the parent:
+//
+//     view.instanceProperty();
+//
+// Inspiration
+// -----------
+//
 // extendable.js is based on [Backbone.js](http://backbonejs.org/) and
 // [Underscore.js](http://underscorejs.org/).
+//
+// About
+// -----
 //
 // Copyright 2012, Kim Joar Bekkelund MIT Licensed
 
 var extendable = (function() {
 
-  // Add all the properties of passed-in object(s) to a given object
+  // Helper method to add all the properties of passed-in object(s) to a
+  // given object
   var add = function(obj) {
     var objects = Array.prototype.slice.call(arguments, 1);
 
@@ -81,6 +122,7 @@ var extendable = (function() {
     return child;
   };
 
+  // The public api
   return function() {
     this.extend = extend;
     return this;
